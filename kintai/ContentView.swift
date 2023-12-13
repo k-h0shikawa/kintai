bd53523243eee1229296c08e63ce599b33f6dc63
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2023-12-13 20:02:15 +0900

    Initial Commit

---
+++ kintai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kintai
+//
+//  Created by hoshikawa on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kintai/kintaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kintaiApp.swift
+//  kintai
+//
+//  Created by hoshikawa on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct kintaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
